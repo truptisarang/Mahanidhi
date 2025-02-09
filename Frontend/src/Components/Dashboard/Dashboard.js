6e5d35b87bb6327a62e711d16106996b54c8eaad
Trupti Sarang <truptisarang0305@gmail.com>
2025-02-10 00:14:02 +0530

    Wallet balance

--- Frontend/src/Components/Dashboard/Dashboard.js
+++ Frontend/src/Components/Dashboard/Dashboard.js
@@ -37,7 +37,7 @@ const Dashboard = () => {
     const walletAddress = await getWallet();
     console.log(walletAddress)
     if(walletAddress){
-        getUSDCBalance(walletAddress)        
+       await getUSDCBalance(walletAddress)        
     }
    }
    fetchWalletBalance();
